02b3f6bfe92014202093d95700cabf9481d61912
Yogesh Varal <yogeshvaralhdp@gmail.com>
2022-01-27 22:31:05 +0530

    Useefferct to load saved images

--- newfrontend/src/App.js
+++ newfrontend/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
@@ -12,18 +13,42 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  const handleSearchSubmit = (e) => {
+  const getSavedImages = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/images`);
+      setImages(res.data || []);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => getSavedImages(), []);
+
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages([{ ...data, title: word }, ...images]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`);
+      setImages([{ ...res.data, title: word }, ...images]);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
     setWord('');
   };
+
+  // old way to write search function without AXIOS
+  // const handleSearchSubmit = (e) => {
+  //   e.preventDefault();
+  //   fetch(`${API_URL}/new-image?query=${word}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setImages([{ ...data, title: word }, ...images]);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  //   setWord('');
+  // };
 
   const handleDelete = (id) => {
     setImages(images.filter((image) => image.id !== id));
